1762c11eff06b8ad4b4a083294ff4f25bbf46ce9
sha <atcha.crypto@gmail.com>
2025-02-21 17:37:19 +0700

    introduce react browser router and style

--- src/App.tsx
+++ src/App.tsx
@@ -8,15 +8,15 @@ function App() {
 
   return (
     <>
-      <div>
+      <div className='flex flex-col'>
         <a href='https://vite.dev' target='_blank'>
-          <img src={viteLogo} className='logo' alt='Vite logo' />
+          <img src={viteLogo} className='logo center-img m-auto' alt='Vite logo' />
         </a>
         <a href='https://react.dev' target='_blank'>
-          <img src={reactLogo} className='logo react' alt='React logo' />
+          <img src={reactLogo} className='logo react m-auto' alt='React logo' />
         </a>
       </div>
-      <h1>Vite + React</h1>
+      <h1>Vite + React Router V7</h1>
       <div className='card'>
         <button onClick={() => setCount((count) => count + 1)}>count is {count}</button>
         <p>
